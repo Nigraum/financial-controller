77fa564d40fd433c9806371bbc43450f807adf1d
Nigraum <yan.corintima@hotmail.com>
2022-10-20 23:49:01 -0300

    Create container for resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+const Resume = () => {
+  return (
+    <Container>
+      <h1>Teste</h1>
+    </Container>
+  );
+};
+
+export default Resume;
