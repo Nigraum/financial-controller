cd9b2f375c6ba97b8ddff0181bd270390bbff2f5
Nigraum <yan.corintima@hotmail.com>
2022-10-21 23:18:29 -0300

    Installing fa icons and testing

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ResumeItem from "../ResumeItem";
 import { Container } from "./style";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
 
 const Resume = () => {
   return (
     <Container>
-      <ResumeItem />
+      <ResumeItem icon={FaRegArrowAltCircleUp} />
       <ResumeItem />
       <ResumeItem />
     </Container>
