c587cd0ad335ca3030c7b40eb6495c53b242e3fd
Nigraum <yan.corintima@hotmail.com>
2022-10-23 01:04:22 -0300

    Starting to set real skeleton for form and create const desc and setDesc

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Input, InputContent, Label, RadioGroup } from "./style";
 
 const Form = () => {
+  const [desc, setDesc] = useState("");
+
   return (
     <Container>
       <InputContent>
-      <Label />
-      <RadioGroup>
-      </RadioGroup>
-      <Input />
-      <Input />
+        <Label>
+          <Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+        </Label>
       </InputContent>
-      <Button />
     </Container>
   );
 };
