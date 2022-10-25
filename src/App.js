557f26e728a15ab7e32a201ff66692dea8d23bd5
Nigraum <yan.corintima@hotmail.com>
2022-10-25 13:56:18 -0300

    Create some consts on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GlobalStyle from "./assets/global";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import Resume from "./components/Resume";
 
 const App = () => {
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data) : []
+  );
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const amountExpense = transactionsList
+    .filter((item) => !item.expense)
+    .map((transaction) => Number(transaction.amount));
+
+    const amountIncome = transactionsList
+    .filter((item) => !item.expense)
+    .map((transaction) => Number(transaction.amount));
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+  }, [transactionsList]);
+
   return (
     <>
       <Header />
